36ee912bc61a0e54e939d8c1af19071f38b74f95
akinabt <abekmambetova364@gmail.com>
2022-08-09 20:30:43 +0600

    class Author

---
+++ author.py
@@ -0,0 +1,40 @@
+import mysql.connector
+
+class Author:
+    def __init__(self, cursor_param):
+        self.cursor = cursor_param
+
+    def create_table_author(self):
+        query = """
+        CREATE TABLE IF NOT EXISTS authors (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(100) NOT NULL,
+        surname VARCHAR(100) NOT NULL
+        );
+        """
+        self.cursor.execute(query)
+        print('table has been created')
+
+    def add_new_author(self, name, surname):
+        query = f"INSERT INTO authors(name, surname)" \
+                f"VALUES" \
+                f"('{name}', '{surname}');"
+        self.cursor.execute(query)
+
+    def get_author_by_id(self, id):
+        query = f"SELECT * FROM authors WHERE id = {id};"
+        self.cursor.execute(query)
+        print(self.cursor.fetchone())
+
+    def get_all_authors(self):
+        query = f"SELECT * FROM authors;"
+        self.cursor.execute(query)
+        print(self.cursor.fetchall())
+
+    def get_author_by_name(self, name):
+        query = f"SELECT name FROM authors WHERE name = '{name}';"
+        self.cursor.execute(query)
+        print(self.cursor.fetchone())
+
+
+
